503873301141041952bffd1994f538b13f127ecf
Damon Morris <d.j.morris@sheffield.ac.uk>
2020-08-17 15:07:19 +0100

    add code to export results to .csv

--- R/impact_calculate.R
+++ R/impact_calculate.R
@@ -4,19 +4,24 @@
 #' output, employment, and gross value added impacts of the policy.
 #'
 #' @param demand.change list object created by `simulate_alcohol_policy()`
+#' @param multipliers data frame of multipliers used to calculate the impacts.
+#' @param export logical; if TRUE saves a csv file of results.
+#' @param path filepath for exporting results.
 #'
 #' @return A table of model outputs
 #'
 #' @export
 
 impact_calculate <- function(demand.change = NULL,
-                             multipliers = NULL) {
+                             multipliers = NULL,
+                             export = FALSE,
+                             path = "output/") {
 
   output.effects.1 <- round(sum(demand.change$final.demand*multipliers[,"output.multipliers.type1"]),3)
   output.effects.2 <- round(sum(demand.change$final.demand*multipliers[,"output.multipliers.type2"]),3)
 
-  empl.effects.1 <- round(sum(demand.change$final.demand*multipliers[,"emp.multipliers.type1"]))
-  empl.effects.2 <- round(sum(demand.change$final.demand*multipliers[,"emp.multipliers.type2"]))
+  empl.effects.1 <- round(sum(demand.change$final.demand*multipliers[,"emp.multipliers.type1"])/1000)
+  empl.effects.2 <- round(sum(demand.change$final.demand*multipliers[,"emp.multipliers.type2"])/1000)
 
   gva.effects.1 <- round(sum(demand.change$final.demand*multipliers[,"gva.multipliers.type1"]),3)
   gva.effects.2 <- round(sum(demand.change$final.demand*multipliers[,"gva.multipliers.type2"]),3)
@@ -30,7 +35,9 @@ impact_calculate <- function(demand.change = NULL,
                    byrow=FALSE,
                    ncol = 3,
                    dimnames = list(c("Type 1","Type 2"),
-                                   c("Output","GVA","Employment")))
-
+                                   c("Output (£bn)","GVA (£bn)","Employment (000s)")))
+if (export == TRUE) {
+  write.csv(output,paste0(path,"example.csv"))
+}
   return(output)
 }
