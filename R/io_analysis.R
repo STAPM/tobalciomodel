89a8ef65b84b78fe72ca6901ffa179a4de11c316
Damon Morris <d.j.morris@sheffield.ac.uk>
2020-08-21 13:49:50 +0100

    finish the wrapper function

--- R/io_analysis.R
+++ R/io_analysis.R
@@ -4,30 +4,67 @@
 #' pre-loaded scenario option.
 #'
 #' @param scenario the row number of the data frame `tobalciomodel::scenarios` corresponding to the chosen scenario to model.
-#'
+#' @param hhold allow households to re-allocate spending
+#' @param hhold.pro.rata arguments to pass to `hhold_reallocate()` - pro rata redistribute
+#' @param hhold.sectors arguments to pass to `hhold_reallocate()` - specific sectors to re-allocate to
+#' @param hhold.prop arguments to pass to `hhold_reallocate()` - proportions to re-allocate by sector
 #' @export
-io.analysis <- function(scenario = NULL) {
+io.analysis <- function(scenario.num = NULL,
+                        hhold = FALSE,
+                        hhold.pro.rata = TRUE,
+                        hhold.sectors = NULL,
+                        hhold.prop = NULL) {
 ### select a pre-loaded scenario
-scenario <- select_scenario(scenario)
+scenario <- select_scenario(scenario.num)
 
-### Select data and parameters
-inputs <- final_demand_inputs(yr =         scenario[["year"]],
-                              scotland =   scenario[["scotland"]],
-                              elasticity = scenario[["elasticity"]])
+alcohol <- ifelse(is.null(scenario[["alc.policy"]]),FALSE,TRUE)
+tobacco <- ifelse(is.null(scenario[["tob.policy"]]),FALSE,TRUE)
 
-### Simulate an alcohol policy
-policy_sim <- simulate_alcohol_policy(data = inputs,
-                                      alc.policy   = scenario[["alc.policy"]],
-                                      on.trade.ch  = scenario[["alc.on.ex"]],
-                                      off.trade.ch = scenario[["alc.off.ex"]],
-                                      prob = FALSE)
+############## Model Alcohol
 
-### Simulate a tobacco policy
+# create empty vector of final demand changes to overwrite
+alc_policy_sim <- rep(0,106)
+
+if (alcohol == TRUE) {
+  ### Extract relevant data and parameters for the chosen scenario
+  inputs <- final_demand_inputs_alc(yr =         scenario[["year"]],
+                                    scotland =   scenario[["scotland"]],
+                                    elasticity = scenario[["alc.elasticity"]])
+
+  ### Simulate the alcohol policy
+  alc_sim <- simulate_alcohol_policy(data = inputs,
+                                           alc.policy   = scenario[["alc.policy"]],
+                                           on.trade.ch  = scenario[["alc.on.ex"]],
+                                           off.trade.ch = scenario[["alc.off.ex"]],
+                                           prob = FALSE)
+  alc_policy_sim <- alc_sim$final.demand
+}
+
+############## Model Tobacco
+tob_policy_sim <- rep(0,106)
+if (tobacco == TRUE) {
+  ### Extract relevant data and parameters for the chosen scenario
+  tob_inputs <- final_demand_inputs_tob(yr =         scenario[["year"]],
+                                        elasticity = scenario[["tob.elasticity"]])
+
+  ### Simulate the alcohol policy
+  tob_policy_sim <- simulate_tobacco_policy(data =         tob_inputs,
+                                            tob.policy   = scenario[["tob.policy"]],
+                                            cigs.ch =      scenario[["tob.ex"]])
+}
+#### Sum up the tobacco and alcohol final demand changes
+
+policy_sim <- alc_policy_sim + tob_policy_sim
 
 
-### Sum joint tobacco/alcohol effects
-
-
+#### reallocate household spending if specified in the function
+if (hhold == TRUE) {
+hhold <- hhold_reallocate(policy_sim,
+                          pro.rata = hhold.pro.rata,
+                          sectors = hhold.sectors,
+                          prop = hhold.prop)
+policy_sim <- hhold
+}
 
 ### Extract the multipliers
 multipliers <- multipliers(yr =    scenario[["year"]],
