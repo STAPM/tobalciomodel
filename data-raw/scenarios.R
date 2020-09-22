33b7459df8fe18688cb80d191a06bd9f94577541
Damon Morris <d.j.morris@sheffield.ac.uk>
2020-09-22 16:35:06 +0100

    update model scenarios spreadsheet

--- data-raw/scenarios.R
+++ data-raw/scenarios.R
@@ -1,5 +1,8 @@
 library(readxl)
 
-scenarios <- read_excel(paste0("data-raw/","scenarios for io model",".xlsx"))
+scenarios <- read_excel(paste0("data-raw/","io model input parameters",".xlsx"),
+                        sheet = "Parameters",
+                        range = "A1:R101",
+                        col_names = TRUE)
 
 usethis::use_data(scenarios,overwrite=TRUE)
