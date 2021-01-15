c26d840a6a9a42b934186334cfeea3660da58014
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-01-15 14:41:38 +0000

    delete redundant files

--- R/data.R
+++ R/data.R
@@ -34,27 +34,6 @@
 #' @format A data frame with 106 observations and 23 variables.
 "employment"
 
-#' Input-Output Table.
-#'
-#' A data frame containing relevant data extracted from the publicly available
-#' alcohol-disaggregated IO table used by the Fraser of Allender Institute.
-#'
-#' @format A data frame with 106 observations and 116 variables.
-#' \describe{
-#'     \item{name}{Name of the SIC07-based sector of economic activity.}
-#'     \item{sec1-sec106}{For each sector, a variable capturing inter-industry sales/purchases.}
-#'     \item{hhold.demand}{The vector of household demand (a component of final demand).}
-#'     \item{final.demand}{The vector of final demands (all non-intermediate demand).}
-#'     \item{hhold.output}{The vector of employee compensation - the value added to total output by household labour supply.}
-#'     \item{total.output}{The vector of total output - value of raw materials, plus imports, returns to capital and labour, taxes.}
-#'     \item{total.demand}{Should equal total output.}
-#'     \item{gva.taxes}{gross value added - taxes less subsidies on production component}
-#'     \item{gva.wages}{gross value added - employee compensation component}
-#'     \item{gva.gos}{gross value added - gross operating surplus component}
-#'     \item{gva.total}{gross value added total - sum of employee compensation, gross operating surplus, and taxes on production}
-#'     }
-"iotable"
-
 #' Macroeconomic Data.
 #'
 #' Total employment generated from the quarterly Labour Force Survey (LFS) by 4-digit SIC-2007 industry. Data
@@ -76,20 +55,4 @@
 #'     \item{gva_real}{Real Gross Value Added in 2010 prices (£millions)}
 #' }
 "macro"
-
-#' Analysis Scenarios
-#'
-#' A data frame containing pre-specified input parameters for scenario analyses.
-#'
-#' @format A data frame with 10 observations and 4 variables.
-#' \describe{
-#'     \item{year}{year of data to use}
-#'     \item{scotland}{TRUE if applying scenario to Scotland}
-#'     \item{elasticity}{source of elasticity information}
-#'     \item{emp.measure}{measure of employment - fte, employment, or hours}
-#'     \item{alc.policy}{alcohol policy scenario - exog, MUP, tax, or NULL for no policy}
-#'     \item{tob.policy}{tobacco policy scenario - exog, MUP, tax, or NULL for no policy}
-#' }
-"scenarios"
-
 
