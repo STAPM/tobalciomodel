4318589bc93fc9665c782df22ebb02a520e78a89
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-05-17 15:04:01 +0100

    add wrapper function for producing the change in final demand vector

---
+++ R/PrepFinalDemand.R
@@ -0,0 +1,47 @@
+#' Setup Final Demand Vector
+#'
+#' Input changes in household and government expenditures, specify assumptions
+#' about the distributions of spending and the household saving rate, and produce
+#' the resulting vector of changes in final demand to model.
+#'
+#' @param hhold_exp Numeric. Change in household consumption measured in basic prices.
+#' @param govt_exp Numeric. Change in government spending.
+#' @param hhold_saving Numeric. Assumed household savings rate.
+#' @param hhold_vector Character. How household spending is redistributed.
+#' @param govt_vector Character. How government spending is redistributed.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+PrepFinalDemand <- function(hhold_exp,
+                            govt_exp,
+                            hhold_saving = 0.1,
+                            hhold_vector = "hhfce_noalctob",
+                            govt_vector = "central") {
+
+  ### Distribute changes in household spending
+
+  final_demand_hhold <- tobalciomodel::ReallocateHhold(expenditure = hhold_exp,
+                                                       saving_rate = hhold_saving,
+                                                       vector = hhold_vector,
+                                                       vectors_data = tobalciomodel::vectors_hhold,
+                                                       mapping = tobalciomodel::coicop_cpa_mapping)
+
+  ### Distribute changes in government spending
+  final_demand_govt  <- tobalciomodel::ReallocateGovt(expenditure = govt_exp,
+                                                      vector = govt_vector,
+                                                      vectors_data = tobalciomodel::vectors_govt)
+
+
+  ### Merge and sum up the household and government vectors to get the
+  ### overall change in final demand vector.
+
+  final_demand <- merge.data.table(final_demand_hhold,
+                                   final_demand_govt,
+                                   by = c("CPA_code","Product"))
+
+  final_demand[, final_demand := hhold_exp + govt_exp]
+
+  return(final_demand)
+}
