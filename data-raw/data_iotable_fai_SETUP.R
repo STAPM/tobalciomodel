7a5b5ff394977a056ba1477a8a4e4ba0efd38370
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-08-24 20:42:41 +0100

    update the FAI data

--- data-raw/data_iotable_fai_SETUP.R
+++ data-raw/data_iotable_fai_SETUP.R
@@ -13,26 +13,34 @@ sheet <- "Sheet1"
 ### Read in the sector names
 sector <- read_excel(path = paste0(path,file),
                      sheet = sheet,
-                     range = "C6:C111",
+                     range = "B6:C111",
                      col_names = FALSE)
+
+setDT(sector)
+
+setnames(sector, names(sector), c("IOC","Sector"))
 
 ### Read in the flowtable
 flowtable <- read_excel(path = paste0(path,file),
                         sheet = sheet,
                         range = "D6:DE111",
                         col_names = FALSE)
+setDT(flowtable)
+
 
 ### Read in the household consumption/demand and output rows
 hhold.output <- read_excel(path = paste0(path,file),
                            sheet = sheet,
                            range = "D117:DE117",
                            col_names = FALSE)
+setDT(hhold.output)
 
 
 hhold.demand <- read_excel(path = paste0(path,file),
                            sheet = sheet,
                            range = "DG6:DG111",
                            col_names = FALSE)
+setDT(hhold.demand)
 
 ### Read in the final demand and total output
 
@@ -40,49 +48,57 @@ total.output <- read_excel(path = paste0(path,file),
                            sheet = sheet,
                            range = "D120:DE120",
                            col_names = FALSE)
+setDT(total.output)
 
 
 final.demand <- read_excel(path = paste0(path,file),
                            sheet = sheet,
                            range = "DS6:DS111",
                            col_names = FALSE)
+setDT(final.demand)
 
 total.demand <- read_excel(path = paste0(path,file),
                            sheet = sheet,
                            range = "DT6:DT111",
                            col_names = FALSE)
+setDT(total.demand)
 
 govt.demand <- read_excel(path = paste0(path,file),
                           sheet = sheet,
                           range = "DI6:DI111",
                           col_names = FALSE)
+setDT(govt.demand)
 
 gva.taxes <- read_excel(path = paste0(path,file),
                         sheet = sheet,
                         range = "D116:DE116",
                         col_names = FALSE)
+setDT(gva.taxes)
 
 gva.wages <- read_excel(path = paste0(path,file),
                         sheet = sheet,
                         range = "D117:DE117",
                         col_names = FALSE)
+setDT(gva.wages)
 
 gva.gos <- read_excel(path = paste0(path,file),
                       sheet = sheet,
                       range = "D118:DE118",
                       col_names = FALSE)
+setDT(gva.gos)
 
 gva.total <- read_excel(path = paste0(path,file),
                         sheet = sheet,
                         range = "D119:DE119",
                         col_names = FALSE)
+setDT(gva.total)
 
 
 flowtable <- as.matrix(flowtable)
-total.demand <- as.matrix(total.demand)
-final.demand <- as.matrix(final.demand)
-govt.demand  <- as.matrix(govt.demand)
-hhold.demand <- as.matrix(hhold.demand)
+total.demand <- as.vector(as.matrix(total.demand))
+final.demand <- as.vector(as.matrix(final.demand))
+govt.demand  <- as.vector(as.matrix(govt.demand))
+hhold.demand <- as.vector(as.matrix(hhold.demand))
 total.output <- as.vector(as.matrix(total.output))
 hhold.output <- as.vector(as.matrix(hhold.output))
 
@@ -91,12 +107,10 @@ gva.wages <- as.vector(as.matrix(gva.wages))
 gva.gos   <- as.vector(as.matrix(gva.gos))
 gva.total <- as.vector(as.matrix(gva.total))
 
-name <- as.vector(as.matrix(sector))
-
-iotable_fai <- data.frame(name,flowtable,hhold.demand,govt.demand,final.demand,hhold.output,total.output,total.demand,
+iotable_fai <- cbind(sector,flowtable,hhold.demand,govt.demand,final.demand,hhold.output,total.output,total.demand,
                                gva.taxes,gva.wages,gva.gos,gva.total)
 
-setnames(iotable_fai, old = names(iotable_fai), new = c("name",paste0("sec",c(1:106)),
+setnames(iotable_fai, old = names(iotable_fai), new = c("IOC","Sector", paste0("sec",c(1:106)),
                                                                   "hhold.demand","govt.demand","final.demand",
                                                                   "hhold.output","total.output","total.demand",
                                                                   "gva.taxes","gva.wages","gva.gos","gva.total"))
