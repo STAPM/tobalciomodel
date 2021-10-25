d58e3d0c8920427d282f01830bf4fe2c5352e1f3
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-10-25 13:01:13 +0100

    add new function for generating output summaries

---
+++ R/ProcessOutputs.R
@@ -0,0 +1,17 @@
+#' Generate Model Outputs
+#'
+#' Take the output of the model and generate summary outputs
+#'
+#' @param data data table. Output of the input-output modelling
+#' @param sort character. Variable on which to sort sectors by effect size. Inputs are one from
+#' c("output","empl","gva","inctaxes","earn")
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ProcessOutputs <- function(data,
+                           sort) {
+
+
+}
