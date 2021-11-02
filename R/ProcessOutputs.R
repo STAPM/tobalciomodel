bcd2e1532b09c2067d47d6e68561dcbd45d88ae7
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-11-02 09:14:29 +0000

    add 1-digit SIC level results to the output processing

--- R/ProcessOutputs.R
+++ R/ProcessOutputs.R
@@ -5,6 +5,8 @@
 #' @param data data table. Output of the input-output modelling
 #' @param sort character. Variable on which to sort sectors by effect size. Inputs are one from
 #' c("output","empl","gva","inctaxes","earn")
+#' @param FAI Logical. If TRUE, uses the Fraser of Allender Institute (FAI) table. If FALSE, uses the
+#'            ONS supply and use tables. Defaults to FALSE
 #'
 #' @return
 #' @export
@@ -15,9 +17,72 @@
 #'
 #' }
 ProcessOutputs <- function(data,
-                           sort) {
+                           sort = NULL,
+                           FAI = FALSE) {
 
   output <- copy(data)
+
+  if (isTRUE(FAI)) {
+    output[1:7   , Industry := "Agriculture and Mining"]
+    output[8:51  , Industry := "Manufacturing"]
+    output[52:57 , Industry := "Utilities"]
+    output[58    , Industry := "Construction"]
+    output[59:61 , Industry := "Wholesale and Retail"]
+    output[62:67 , Industry := "Transport and Storage"]
+    output[68:71 , Industry := "Accommodation and Food Services"]
+    output[72:76 , Industry := "Information and Communication"]
+    output[77:106, Industry := "Other Services"]
+  } else if (!isTRUE(FAI)) {
+    output[1:7   , Industry := "Agriculture and Mining"]
+    output[8:50  , Industry := "Manufacturing"]
+    output[51:56 , Industry := "Utilities"]
+    output[57    , Industry := "Construction"]
+    output[58:60 , Industry := "Wholesale and Retail"]
+    output[61:66 , Industry := "Transport and Storage"]
+    output[67:68 , Industry := "Accommodation and Food Services"]
+    output[69:73 , Industry := "Information and Communication"]
+    output[74:105, Industry := "Other Services"]
+  }
+
+  ###############################
+  ## INDUSTRY LEVEL FIGURES #####
+
+  output[, Industry := factor(Industry, levels = c("Agriculture and Mining","Manufacturing","Utilities",
+                                                   "Construction","Wholesale and Retail","Transport and Storage",
+                                                   "Accommodation and Food Services","Information and Communication","Other Services"))]
+
+  output_ind <- output[, .(out_0 = sum(out_effects_t0_p),
+                           out_1 = sum(out_effects_t1_p)), by = "Industry"]
+  output_ind[,out_t := out_0 + out_1]
+  output_ind[,outcome := "Output"]
+
+  gva_ind    <- output[, .(out_0 = sum(gva_effects_t0_p),
+                           out_1 = sum(gva_effects_t1_p)), by = "Industry"]
+  gva_ind[,out_t := out_0 + out_1]
+  gva_ind[,outcome := "GVA"]
+
+  empl_ind   <- output[, .(out_0 = sum(emp_effects_t0_p),
+                           out_1 = sum(emp_effects_t1_p)), by = "Industry"]
+  empl_ind[,out_t := out_0 + out_1]
+  empl_ind[,outcome := "Employment"]
+
+  earn_ind   <- output[, .(out_0 = sum(netearn_effects_t0_p),
+                           out_1 = sum(netearn_effects_t1_p)), by = "Industry"]
+  earn_ind[,out_t := out_0 + out_1]
+  earn_ind[,outcome := "Net Earnings"]
+
+  inctax_ind <- output[, .(out_0 = sum(emptax_effects_t0_p),
+                           out_1 = sum(emptax_effects_t1_p)), by = "Industry"]
+  inctax_ind[,out_t := out_0 + out_1]
+  inctax_ind[,outcome := "Income Tax"]
+
+  results_ind <- rbindlist(list(output_ind,gva_ind,empl_ind,
+                                earn_ind,inctax_ind))
+
+  setnames(results_ind, names(results_ind), c("Industry","Direct Effect","Indirect Effect","Total Effect","Outcome"))
+
+  ###############################
+  ## AGGREGATE FIGURES ##########
 
   ## generate total type 0 and type 1 effects for output, GVA, employment,
   ## earnings, and income tax on earnings
@@ -57,5 +122,6 @@ ProcessOutputs <- function(data,
 
   ### output the results
 
-  return(tab)
+  return(list(aggregate = tab,
+              industry = results_ind))
 }
